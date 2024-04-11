8175ded0244922370a7285923f8c3803f1ca522f
brian <briantaveras99@gmail.com>
2024-04-10 21:47:36 -0400

    Initial commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const outputTextElement = document.querySelector('#text-output');
 const buttonElement = document.querySelector('button');
 const invCookies = document.querySelector('#cookie-list');
 const playerHpElement = document.querySelector('#player-hp');
+const outputElement = document.querySelector('#output');
 const cookiesAvailable = ['chocolate', 'oatmeal', 'macademia', 'vanilla', 'chocolate-chip'];
 const player = {
     hp: 3,
@@ -14,15 +15,22 @@ function randomCookie() {
 }
 
 function clickHandler() {
-    if (outputTextElement.innerHTML.includes('You got a Cookie!')) {
-        outputTextElement.innerHTML = "starting over!";
+    if (outputTextElement.innerHTML.includes('You got a Cookie!') && (player.hp > 0) ){
+        outputTextElement.innerHTML = "try again!";
+    } else if(player.hp <= 0){
+        player.hp = 0;
+        outputTextElement.innerHTML = `Oh no!<br><br>You lost!!`;
     } else {
         const cookie = randomCookie();
         outputTextElement.innerHTML = 'You got a Cookie!<br><br>' + cookie;
         addToInventory(cookie);
     }
-    console.log(player.hp);
+    if (checkSameCookieCount(player.inventory) >= 3) {
+        outputTextElement.innerHTML = `Congratulations!<br><br>You won!!`;
+    }
 }
+
+
 
 function addToInventory(cookie) {
     player.inventory.push(cookie);
@@ -35,5 +43,15 @@ function addToInventory(cookie) {
         playerHpElement.textContent = `HP: ${player.hp}`; // Update HP display
     }
 }
+function checkSameCookieCount(inventory) {
+    const cookieCounts = {};
+    let maxCount = 0;
 
+    for (const cookie of inventory) {
+        cookieCounts[cookie] = (cookieCounts[cookie] || 0) + 1;
+        maxCount = Math.max(maxCount, cookieCounts[cookie]);
+    }
+
+    return maxCount;
+}
 buttonElement.addEventListener('click', clickHandler);
