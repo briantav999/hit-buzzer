463c10948af73d263225480daeccff6e5f93b608
brian <briantaveras99@gmail.com>
2024-04-12 20:28:14 -0400

    Initial commit

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const outputTextElement = document.querySelector('#text-output');
+const outputScreenElement = document.querySelector('#screen')
 const buttonElement = document.querySelector('button');
 const invCookies = document.querySelector('#cookie-list');
 const playerHpElement = document.querySelector('#player-hp');
 const outputElement = document.querySelector('#output');
-const cookiesAvailable = ['chocolate', 'oatmeal', 'macademia', 'vanilla', 'chocolate-chip'];
+const cookiesAvailable = ['chocolate', 'oatmeal', 'macademia', 'vanilla', 'chocolatechip'];
 const cookieImages = {
-    chocolate: "https://i.imgur.com/M7chATe.jpeg"
+    chocolate: "https://media.istockphoto.com/id/1046582724/photo/close-up-image-of-soft-baked-dark-chocolate-cookies.jpg?s=612x612&w=0&k=20&c=jeP79ypxgj3s7oiMpdrHFpShzUMAJemxh9Je2LzfMKA=",
+    vanilla: "https://media.istockphoto.com/id/1270135720/photo/vanilla-wafer-stack.jpg?s=1024x1024&w=is&k=20&c=9U4ZL3J6M9sg2Dd7XLKzVV8n1WsbLGztx7lcArBMsOw=",
+    oatmeal: "https://media.istockphoto.com/id/1089728238/photo/oatmeal-cookies-with-glass-of-milk.jpg?s=612x612&w=0&k=20&c=TrtPGRyDrUBaw21iae_YhqYro9zdARDnlpuv7KPb6-I=",
+    chocolatechip: "https://media.istockphoto.com/id/517109442/photo/chocolate-chip-cookie-isolated.jpg?s=612x612&w=0&k=20&c=RgZOYwzVRTXnIBy8zSkXK-wJfNBy9w023UGULkbH_VE=",
+    macademia: "https://media.istockphoto.com/id/175535621/photo/macadamia-nut-and-white-chocolate-cookies.jpg?s=612x612&w=0&k=20&c=Sujp59JmfNkjHGUgQ3iIBGwhQk3TejJJyvfyA3vPznY="
 }
 const player = {
     hp: 3,
@@ -18,17 +23,20 @@ function randomCookie() {
 }
 
 function clickHandler() {
-    if (outputTextElement.innerHTML.includes('You got a Cookie!') && (player.hp > 0) ){
+    if (outputTextElement.innerHTML.includes('You got a Cookie!') && (player.hp > 0)) {
         outputTextElement.innerHTML = "try again!";
-    } else if(player.hp <= 0){
+    } else if (player.hp <= 0) {
         player.hp = 0;
         outputTextElement.innerHTML = `Oh no!<br><br>You lost!!`;
     } else {
         const cookie = randomCookie();
-        const imageUrl = cookieImages[cookie];
-        outputTextElement.innerHTML = `You got a Cookie!<br><br><img src="${imageUrl}" alt="${cookie} Cookie"> ${cookie}`;
+        const img = document.createElement("img");
+        img.src = cookieImages[cookie];
+        outputTextElement.innerHTML = `You got a Cookie!<br><br>${cookie}`;
+        outputTextElement.appendChild(img); // Append the image element, not the URL
         addToInventory(cookie);
     }
+
     if (checkSameCookieCount(player.inventory) >= 3) {
         outputTextElement.innerHTML = `Congratulations!<br><br>You won!!`;
     }
